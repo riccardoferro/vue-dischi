e386d321695ee354b59af8bfdc138d3ff6ce7814
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-08 18:23:43 +0200

    import library axios and get the API in input for analize his datas

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,42 @@
 <template>
-  <main>
-    
-
-  </main>
+    <main>
+    </main>
 </template>
 
 <script>
+// import library axios
+import axios from 'axios';
+
 export default {
-  name: "ComponentMain"
+  name: "ComponentMain",
+  data(){
+    return {
+      // created array albums where we gonna put the datas
+      albums:[]
+    }
+  },
+  //here we gonna take the url API
+  props: {
+    url: String
+  },
+  // when the software si ready go with this first function
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    loadData(){
+      // here we gonna ask for a response from the api
+      axios.get(this.url).then(
+        (response)=> {
+          // if response good
+          if (response.status === 200) {
+              this.albums = response.data
+              console.log(this.albums)
+          }
+        }
+      )
+    }
+  }
 }
 </script>
 
