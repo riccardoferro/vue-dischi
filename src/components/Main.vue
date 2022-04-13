7f2c4cbf0e8d365c7cbdfeaa7a3f1d5b4759a5c0
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-13 17:05:10 +0200

    add select filter

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,50 +1,107 @@
 <template>
     <main>
-        <div class="container pb-5 pt-5 d-flex justify-content-center">
-            <div >
-                <div v-if="albums.length > 0 " class="row justify-content-between w-75 m-auto">
-                  <SingleAlbum  
-                  v-for="(item,index) in albums" 
-                  :key="index"   
-                  :album = "item" 
-                  class="col-12 col-md-3 col-xl-2 "/>
-                </div>
-                <div v-else class="load" >
-                    <LoadingComponent />
-                </div>
+        <!-- Here we have the select input that filter the albums by gender -->
+        <div class="container ">
+            <div class="row w-75 pt-2 m-auto c-white text-center">
+                    <!-- Call sub-component with a waiting event and we send him "generi='genres'"
+                        where genres is a computed propertie, scroll down to see 
+                     -->
+                    <FilterGenre @changedGenre="performfilteredGenres" :generi="genres"/>
+            </div>
+        </div>
+
+        <!-- Albums with their info -->
+        <div class="container pb-1 pt-1 d-flex justify-content-center">
+            <div v-if="albums.length > 0" class="row gap-2 w-75 m-auto">
+              <!-- We had created a sub-component where we send the single album " :album='item' " -->
+              <SingleAlbum  
+              v-for="(item,index) in filteredGenres" 
+              :key="index"   
+              :album = "item" 
+              class="col-12 col-md-3 col-xl-2 "/>
+            </div>
+
+            <!-- Here we insert an v-else, till the albums are not shown we show an custom loader -->
+            <div v-else class="load" >
+                <LoadingComponent />
             </div>
         </div>
     </main>
 </template>
 
+
 <script>
 // import library axios
 import axios from 'axios';
+
+// import sub-components
 import SingleAlbum from "@/components/subcomponents/SingleAlbum.vue"
 import LoadingComponent from "@/components/subcomponents/LoadingComponent.vue"
+import FilterGenre from "@/components/subcomponents/FilterGenre.vue"
 
 
 export default {
   name: "ComponentMain",
+  
+  // datas
   data(){
     return {
       // created array albums where we gonna put the datas
-      albums:[]
+      albums:[],
+
+      //here we have a var settle to empty
+      selectedGenre: ""
     }
   },
+  // computed properties
+  computed: {
+
+      // here with the function genres we eliminate duplicates from the albums and take only the unique gender of the albums
+      genres(){
+        //other way to take only the genre we need in the filter
+        // const uniqueGenres = [];
+        // this.albums.forEach((item)=>{
+        //   if (!uniqueGenres.includes(item.genre)){
+        //     uniqueGenres.push(item.genre);
+        //   }
+        // })
+        // console.log(genres);
+
+        //trick to eliminate duplicate
+        const genres = this.albums.map((album) => album.genre);
+        const uniqueGenres = [...new Set(genres)];
+        console.log(uniqueGenres)
+        return uniqueGenres;
+      },
+
+      // this function filtered the albums by selectedGenre
+      filteredGenres(){
+        // if empty select all
+        if (this.selectedGenre === ""){
+          return this.albums;
+        }
+        // else select the selectedGenre
+        return this.albums.filter(album => album.genre === this.selectedGenre);
+      }
+  },
+
   //here we gonna take the url API
   props: {
     url: String
   },
+  
   components: {
     SingleAlbum,
-    LoadingComponent
+    LoadingComponent,
+    FilterGenre
   },
-  // when the software si ready go with this first function
+  // when the software si ready go with this first method
   mounted() {
     this.loadData();
   },
+
   methods: {
+    // this function add elements of the APIs in our empty var album
     loadData(){
       // here we gonna ask for a response from the api
       axios.get(this.url).then(
@@ -56,19 +113,28 @@ export default {
               console.log(this.albums)
           }
         }
+      // here we catch an error if we have
       ).catch((error)=>{
         console.log(error);
       })
+    },
+
+    // this method take as a parametrer the value of the emit, in this selectGenre that we put in our
+    // var empty that is call in the same way
+    performfilteredGenres(selectGenre) {
+        this.selectedGenre = selectGenre;
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  main {
-    background-color: hsl(209deg 33% 17%);
-    .load {
-      height: 90vh;
+    main {
+      .load {
+        height: 90vh;
+      }
     }
-  }
+    .c-white {
+      color: white;
+    }
 </style>
---
+++ src/components/subcomponents/FilterGenre.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- v-model="selectedGenre" take the value of the option selected -->
+    <!-- "@change" is an event that when we select other option of the select we call a method  -->
+    <select v-model="selectedGenre" @change="changeGenre">
+        <option value="">All genre</option>
+        <option v-for="genre in generi" :key="genre" :value="genre">
+          {{genre}}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+  name: "GenreFilter",
+
+  // computed properties
+  props: {
+      generi: Array
+  },
+
+  // datas
+  data(){
+    return {
+      // var selectedGenre settled empty, when empty we show all the cards
+      selectedGenre: ""
+    }
+  },
+  
+  // methods
+  methods: {
+    // this method with emit send the value of the var selectedGenre to the father components who is waiting
+    changeGenre(){
+      console.log(this.selectedGenre);
+      this.$emit('changedGenre',this.selectedGenre);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
